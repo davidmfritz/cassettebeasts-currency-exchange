dbc3cf5000ee886a3e86c8608b00bee3916bb5c6
Dave <16521341+davidmfritz@users.noreply.github.com>
2023-05-25 13:06:33 +0200

    Added enabled toggle in settings

--- mods/bloodyscythe_currency_exchange/ModContentInfo.gd
+++ mods/bloodyscythe_currency_exchange/ModContentInfo.gd
@@ -1,10 +1,25 @@
 extends ContentInfo
 
+var setting_toggle_enabled: bool = true
+
+const MODUTILS: Dictionary = {
+	"settings": [
+		{
+			"property": "setting_toggle_enabled",
+			"type": "toggle",
+			"label": "Enabled",
+		},
+	],
+}
+
 func init_content() -> void:
 	assert(DLC.has_mod("cat_modutils", 0))
 	var modutils: ContentInfo = DLC.mods_by_id["cat_modutils"]
 	modutils.callbacks.connect_scene_ready("res://world/maps/interiors/TownHall.tscn", self, "_on_TownHall_ready")
 
 func _on_TownHall_ready(scene: Spatial) -> void:
+	if !setting_toggle_enabled:
+		return
+	
 	var mod_scene: Spatial = preload("CustomTownHallSpawn.tscn").instance()
 	scene.add_child(mod_scene)
